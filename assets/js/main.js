5bc9afc9747bdab3586bcfeeadbc859209dc68e2
SHAIK-MUNEERUDDIN <120248240+SHAIK-MUNEERUDDIN@users.noreply.github.com>
2023-04-13 18:09:36 +0530

    added js to close toggle menu close in nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
 function clickSingleA(a) {
     items = document.querySelectorAll('.single.active');
+    checkbox = document.getElementById("toggle-1");
 
     if (items.length) {
         items[0].className = 'single';
+        checkbox.checked = false;
     }
 
     a.className = 'single active';
+
 }
