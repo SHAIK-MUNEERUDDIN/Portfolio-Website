1624b465f7a2865131c1b2ceac38abde1f249fb6
SHAIK-MUNEERUDDIN <120248240+SHAIK-MUNEERUDDIN@users.noreply.github.com>
2023-04-13 13:00:02 +0530

    added js functions to the nav links

--- main.js
+++
@@ -1,9 +0,0 @@
-function clickSingleA(a) {
-    items = document.querySelectorAll('.single.active');
-
-    if (items.length) {
-        items[0].className = 'single';
-    }
-
-    a.className = 'single active';
-}
