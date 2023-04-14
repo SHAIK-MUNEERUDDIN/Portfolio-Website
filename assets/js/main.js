f33723141772bf873a08d9f5f7421fd6448f60b3
SHAIK-MUNEERUDDIN <120248240+SHAIK-MUNEERUDDIN@users.noreply.github.com>
2023-04-14 18:34:22 +0530

    fixed issue-js is executing before jquery loads

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,50 @@
-//javascript to navigate on home section on reload
-window.onload = function () {
-    window.setTimeout(
-        function () { window.scrollTo(0, 0); },
-        10
-    );
-};
+
+//this function makes the script to execte when the page is loaded completely
+$(document).ready(function () {
 
 
 
 
 
-//javascript to make nav link active on click
-function clickSingleA(a) {
-    items = document.querySelectorAll('.single.active');
-    checkbox = document.getElementById("toggle-1");
+    //javascript to navigate on home section on reload
+    window.onload = function () {
+        window.setTimeout(
+            function () { window.scrollTo(0, 0); },
+            10
+        );
+    };
 
-    if (items.length) {
-        items[0].className = 'single';
-        checkbox.checked = false;
+
+
+
+
+    //javascript to make nav link active on click
+    function clickSingleA(a) {
+        items = document.querySelectorAll('.single.active');
+        checkbox = document.getElementById("toggle-1");
+
+        if (items.length) {
+            items[0].className = 'single';
+            checkbox.checked = false;
+        }
+
+        a.className = 'single active';
+
     }
 
-    a.className = 'single active';
 
-}
-
-
-// js to Highlight active section in navbar
-$(document).ready(function () {
-    $(window).scroll(function () {
-        $('section').each(function () {
-            if ($(window).scrollTop() >= $(this).offset().top - 300) {
-                var id = $(this).attr('id');
-                $('nav li a').removeClass('active');
-                $('nav').find('a[href="#' + id + '"]').addClass('active');
-            }
+    // js to Highlight active section in navbar
+    $(document).ready(function () {
+        $(window).scroll(function () {
+            $('section').each(function () {
+                if ($(window).scrollTop() >= $(this).offset().top - 300) {
+                    var id = $(this).attr('id');
+                    $('nav li a').removeClass('active');
+                    $('nav').find('a[href="#' + id + '"]').addClass('active');
+                }
+            });
         });
     });
+
+
 });
