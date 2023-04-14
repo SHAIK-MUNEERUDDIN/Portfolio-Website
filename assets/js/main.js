b157f7944406443140c0f29cb8f228fa8030d001
SHAIK-MUNEERUDDIN <120248240+SHAIK-MUNEERUDDIN@users.noreply.github.com>
2023-04-14 18:26:52 +0530

    added js to make a section active based on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
+//javascript to navigate on home section on reload
+window.onload = function () {
+    window.setTimeout(
+        function () { window.scrollTo(0, 0); },
+        10
+    );
+};
+
+
+
+
+
+//javascript to make nav link active on click
 function clickSingleA(a) {
     items = document.querySelectorAll('.single.active');
     checkbox = document.getElementById("toggle-1");
@@ -10,3 +23,17 @@ function clickSingleA(a) {
     a.className = 'single active';
 
 }
+
+
+// js to Highlight active section in navbar
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('section').each(function () {
+            if ($(window).scrollTop() >= $(this).offset().top - 300) {
+                var id = $(this).attr('id');
+                $('nav li a').removeClass('active');
+                $('nav').find('a[href="#' + id + '"]').addClass('active');
+            }
+        });
+    });
+});
